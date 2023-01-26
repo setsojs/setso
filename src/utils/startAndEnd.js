ace98cf9d9f681f2bdcb8599783c8bdff00370dd
micziz <miczicontent@gmail.com>
2023-01-26 19:32:57 +0100

    Clean up usless code

--- src/utils/startAndEnd.js
+++ src/utils/startAndEnd.js
@@ -1,6 +1,5 @@
-export function start(title, css) {
-    if (css !== undefined) {
-        return `
+export function start(title) {
+    return `
 <html>
   <head>
     <meta charset="UTF-8">
@@ -11,18 +10,6 @@ export function start(title, css) {
   </head>
   <body> 
   `;
-    } else {
-        return `
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>${title}</title>
-  </head>
-  <body> 
-  `;
-    }
 }
 
 export function end() {
