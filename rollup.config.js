3204a09f12f0d103b2c558e52f2baf8f3c3d0df7
micziz <miczicontent@gmail.com>
2023-01-29 18:44:39 +0100

    Better organized and make bundle sooooo smaller

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+import { terser } from 'rollup-plugin-terser'
+
 export default {
     input: 'src/index.js',
     output: {
-        file: 'bundle.js',
+        file: './bundle/bundle.js',
         format: 'es',
+        plugins: [
+            terser({
+              ecma: 2020,
+              mangle: { toplevel: true },
+              compress: {
+                module: true,
+                toplevel: true,
+                unsafe_arrows: true
+              },
+              output: { quote_style: 1 }
+            })
+          ]
     },
 };
