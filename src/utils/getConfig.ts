0a6be981b741f291bb636245d0a6cf9e565ddb59
micziz <miczicontent@gmail.com>
2023-02-15 20:52:23 +0100

    Add comments to getConifg.ts and remove unnecerery code

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -1,10 +1,17 @@
+// Imports
+// Import cwd from process to get the current working directory
 import { cwd } from "process";
 
+// Export an async functions that return a Promise<any> (sigh)
 export async function getConfig(): Promise<any> {
+    // Try
     try {
+        // to import the config, and return the object
         const config = await import(`${cwd()}/setso.config.js`);
         return config;
-    } catch (err) {
+    // If no config is found
+    } catch {
+        // Log the err.
         console.log("No config found");
     }
 }
