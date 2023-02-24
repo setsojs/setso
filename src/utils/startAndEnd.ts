e72619994c0ba071ce9843e1bb9285e02b48f579
micziz <miczicontent@gmail.com>
2023-02-24 15:16:57 +0200

    Markup cleanup

--- src/utils/startAndEnd.ts
+++ src/utils/startAndEnd.ts
@@ -22,7 +22,6 @@ export function start(title: string): string {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bundle.css">
     <title>${title}</title>
   </head>
   <body> 
